79a7c109b29f79f69e6eeede99f3125965f80a89
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-18 00:57:04 -0300

    fix: IResponse interface

--- src/interfaces/IResponse.ts
+++ src/interfaces/IResponse.ts
@@ -1,13 +1,18 @@
-export default interface IResponse {
-  data: {
-    get: string;
-    parameters: unknown[];
-    errors: unknown[];
-    results: number;
-    paging: {
-      current: number;
-      total: number;
-    };
-    response: unknown[];
+interface IData {
+  get: string;
+  parameters: unknown[];
+  errors: {
+    token: string;
   };
+  results: number;
+  paging: {
+    current: number;
+    total: number;
+  };
+  response: unknown[];
+}
+
+export default interface IResponse {
+  data: IData;
+  status: number;
 }
---
+++ src/interfaces/index.ts
@@ -0,0 +1,2 @@
+export type { default as ICountry } from './ICountry';
+export type { default as IResponse } from './IResponse';
