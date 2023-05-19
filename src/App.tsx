526280a19a95b8f83e38fac2745ee45def886bc2
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-18 22:18:16 -0300

    style: add global style to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route } from 'react-router-dom';
 import { FootballProvider } from './contexts/FootballContext';
 import { Login } from './pages/Login';
 import { SearchTeam } from './pages/SearchTeam';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 export default function App() {
   return (
     <FootballProvider>
+      <GlobalStyles />
       <div>
         <Route exact path="/" component={Login} />
         <Route exact path="/busca-time" component={SearchTeam} />
