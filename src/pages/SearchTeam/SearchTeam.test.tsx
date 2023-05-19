d55b8885a789295e6c596edd6b0d6d2e77458037
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-19 00:10:04 -0300

    test: leagues select field

--- src/pages/SearchTeam/SearchTeam.test.tsx
+++ src/pages/SearchTeam/SearchTeam.test.tsx
@@ -4,8 +4,28 @@ import App from '../../App';
 import { mockCountries, mockSeasons } from '../../tests/mocks/';
 import renderWithRouter from '../../tests/utils/renderWithRouter';
 
+jest.mock('../../utils', () => ({
+  fetchLeagueByCountryAndSeason: jest.fn(),
+}));
+
 describe('SearchTeam | page | integration test', () => {
-  it('should render with success', async () => {
+  it('should render countries select field', () => {
+    localStorage.setItem('countries', JSON.stringify(mockCountries));
+    renderWithRouter(<App />, 'busca-time');
+
+    const countriesSelect = screen.getByTestId(
+      'select-countries'
+    ) as HTMLSelectElement;
+
+    expect(countriesSelect).toBeInTheDocument();
+
+    mockCountries.forEach((country) => {
+      userEvent.selectOptions(countriesSelect, country.name);
+      expect(countriesSelect.value).toBe(country.name);
+    });
+  });
+
+  it('should render seasons select field', () => {
     localStorage.setItem('countries', JSON.stringify(mockCountries));
     localStorage.setItem('seasons', JSON.stringify(mockSeasons));
 
@@ -19,17 +39,23 @@ describe('SearchTeam | page | integration test', () => {
       'select-seasons'
     ) as HTMLSelectElement;
 
-    expect(countriesSelect).toBeInTheDocument();
     expect(seasonsSelect).toBeInTheDocument();
 
-    mockCountries.forEach((country) => {
-      userEvent.selectOptions(countriesSelect, country.name);
-      expect(countriesSelect.value).toBe(country.name);
-    });
+    userEvent.selectOptions(countriesSelect, mockCountries[0].name);
 
     mockSeasons.forEach((season) => {
       userEvent.selectOptions(seasonsSelect, season.toString());
       expect(seasonsSelect.value).toBe(season.toString());
     });
+  });
+
+  it('should render leagues select field', async () => {
+    renderWithRouter(<App />, 'busca-time');
+
+    const leaguesSelect = screen.getByTestId(
+      'select-leagues'
+    ) as HTMLSelectElement;
+
+    expect(leaguesSelect).toBeInTheDocument();
   });
 });
