0f1dbd54c872f899f6ed769bd72267ff46353204
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-20 18:43:52 -0300

    feat: team interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export type { default as ICountry } from './ICountry';
 export type { default as ILeague } from './ILeague';
 export type { default as IResponse } from './IResponse';
 export type { default as ISeason } from './ISeason';
+export type { default as ITeam } from './ITeam';
