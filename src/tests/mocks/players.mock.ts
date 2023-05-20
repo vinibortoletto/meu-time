41bb9e9701f97265ce6cd23b59fdcd9645e5f6b1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-20 19:44:40 -0300

    chore: players mock

--- src/tests/mocks/players.mock.ts
+++ src/tests/mocks/players.mock.ts
@@ -1,95 +1,192 @@
-export const mockPlayers = {
-  player: {
-    id: 9902,
-    name: 'Jailson Marcelino dos Santos',
-    firstname: 'Jailson Marcelino',
-    lastname: 'dos Santos',
-    age: 41,
-    birth: {
-      date: '1981-07-20',
-      place: 'São José dos Campos',
-      country: 'Brazil',
-    },
-    nationality: 'Brazil',
-    height: '187 cm',
-    weight: '95 kg',
-    injured: false,
-    photo: 'https://media-1.api-sports.io/football/players/9902.png',
-  },
-  statistics: [
-    {
-      team: {
-        id: 121,
-        name: 'Palmeiras',
-        logo: 'https://media-3.api-sports.io/football/teams/121.png',
-      },
-      league: {
-        id: 71,
-        name: 'Serie A',
+export const mockPlayers = [
+  {
+    player: {
+      id: 9902,
+      name: 'Jailson Marcelino dos Santos',
+      firstname: 'Jailson Marcelino',
+      lastname: 'dos Santos',
+      age: 41,
+      birth: {
+        date: '1981-07-20',
+        place: 'São José dos Campos',
         country: 'Brazil',
-        logo: 'https://media-2.api-sports.io/football/leagues/71.png',
-        flag: 'https://media-2.api-sports.io/flags/br.svg',
-        season: 2015,
       },
-      games: {
-        appearences: 0,
-        lineups: 0,
-        minutes: 0,
-        number: null,
-        position: 'Goalkeeper',
-        rating: null,
-        captain: false,
-      },
-      substitutes: {
-        in: 0,
-        out: 0,
-        bench: 13,
-      },
-      shots: {
-        total: null,
-        on: null,
-      },
-      goals: {
-        total: 0,
-        conceded: null,
-        assists: null,
-        saves: null,
-      },
-      passes: {
-        total: null,
-        key: null,
-        accuracy: null,
-      },
-      tackles: {
-        total: null,
-        blocks: null,
-        interceptions: null,
-      },
-      duels: {
-        total: null,
-        won: null,
-      },
-      dribbles: {
-        attempts: null,
-        success: null,
-        past: null,
-      },
-      fouls: {
-        drawn: null,
-        committed: null,
-      },
-      cards: {
-        yellow: 0,
-        yellowred: 0,
-        red: 0,
-      },
-      penalty: {
-        won: null,
-        commited: null,
-        scored: null,
-        missed: null,
-        saved: null,
-      },
+      nationality: 'Brazil',
+      height: '187 cm',
+      weight: '95 kg',
+      injured: false,
+      photo: 'https://media-1.api-sports.io/football/players/9902.png',
     },
-  ],
-};
+    statistics: [
+      {
+        team: {
+          id: 121,
+          name: 'Palmeiras',
+          logo: 'https://media-3.api-sports.io/football/teams/121.png',
+        },
+        league: {
+          id: 71,
+          name: 'Serie A',
+          country: 'Brazil',
+          logo: 'https://media-2.api-sports.io/football/leagues/71.png',
+          flag: 'https://media-2.api-sports.io/flags/br.svg',
+          season: 2015,
+        },
+        games: {
+          appearences: 0,
+          lineups: 0,
+          minutes: 0,
+          number: null,
+          position: 'Goalkeeper',
+          rating: null,
+          captain: false,
+        },
+        substitutes: {
+          in: 0,
+          out: 0,
+          bench: 13,
+        },
+        shots: {
+          total: null,
+          on: null,
+        },
+        goals: {
+          total: 0,
+          conceded: null,
+          assists: null,
+          saves: null,
+        },
+        passes: {
+          total: null,
+          key: null,
+          accuracy: null,
+        },
+        tackles: {
+          total: null,
+          blocks: null,
+          interceptions: null,
+        },
+        duels: {
+          total: null,
+          won: null,
+        },
+        dribbles: {
+          attempts: null,
+          success: null,
+          past: null,
+        },
+        fouls: {
+          drawn: null,
+          committed: null,
+        },
+        cards: {
+          yellow: 0,
+          yellowred: 0,
+          red: 0,
+        },
+        penalty: {
+          won: null,
+          commited: null,
+          scored: null,
+          missed: null,
+          saved: null,
+        },
+      },
+    ],
+  },
+  {
+    player: {
+      id: 80329,
+      name: 'Ayrton',
+      firstname: 'Ayrton Luiz',
+      lastname: 'Ganino',
+      age: 36,
+      birth: {
+        date: '1985-04-19',
+        place: 'Piracicaba',
+        country: 'Brazil',
+      },
+      nationality: 'Brazil',
+      height: '176 cm',
+      weight: '73 kg',
+      injured: false,
+      photo: 'https://media-2.api-sports.io/football/players/80329.png',
+    },
+    statistics: [
+      {
+        team: {
+          id: 121,
+          name: 'Palmeiras',
+          logo: 'https://media-1.api-sports.io/football/teams/121.png',
+        },
+        league: {
+          id: 71,
+          name: 'Serie A',
+          country: 'Brazil',
+          logo: 'https://media-3.api-sports.io/football/leagues/71.png',
+          flag: 'https://media-1.api-sports.io/flags/br.svg',
+          season: 2015,
+        },
+        games: {
+          appearences: 3,
+          lineups: 1,
+          minutes: 115,
+          number: null,
+          position: 'Defender',
+          rating: '6.850000',
+          captain: false,
+        },
+        substitutes: {
+          in: 2,
+          out: 0,
+          bench: 4,
+        },
+        shots: {
+          total: 8,
+          on: 2,
+        },
+        goals: {
+          total: 0,
+          conceded: null,
+          assists: null,
+          saves: null,
+        },
+        passes: {
+          total: 152,
+          key: 2,
+          accuracy: 86,
+        },
+        tackles: {
+          total: null,
+          blocks: null,
+          interceptions: 18,
+        },
+        duels: {
+          total: 29,
+          won: 12,
+        },
+        dribbles: {
+          attempts: 2,
+          success: 0,
+          past: null,
+        },
+        fouls: {
+          drawn: 5,
+          committed: 3,
+        },
+        cards: {
+          yellow: 1,
+          yellowred: 0,
+          red: 0,
+        },
+        penalty: {
+          won: null,
+          commited: null,
+          scored: 0,
+          missed: 0,
+          saved: null,
+        },
+      },
+    ],
+  },
+];
