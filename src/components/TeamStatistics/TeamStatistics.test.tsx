a6f45059ff349421eb3c3294a97007d7b1e0ce13
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-21 19:46:50 -0300

    test: team statistics titles

--- src/components/TeamStatistics/TeamStatistics.test.tsx
+++ src/components/TeamStatistics/TeamStatistics.test.tsx
@@ -4,9 +4,18 @@ import TeamStatistics from './TeamStatistics';
 describe('TeamStatistics | component | unit test', () => {
   it('should render with success', () => {
     render(<TeamStatistics />);
+    const playersTitle = screen.getByRole('heading', { name: /jogadores/i });
+    const formationTitle = screen.getByRole('heading', {
+      name: /formação mais utilizada/i,
+    });
+    const resultsTitle = screen.getByRole('heading', { name: /resultados/i });
+    const goalsTitle = screen.getByRole('heading', {
+      name: /gols por tempo de jogo/i,
+    });
 
-    expect(
-      screen.getByRole('heading', { name: 'Jogadores' })
-    ).toBeInTheDocument();
+    expect(playersTitle).toBeInTheDocument();
+    expect(formationTitle).toBeInTheDocument();
+    expect(resultsTitle).toBeInTheDocument();
+    expect(goalsTitle).toBeInTheDocument();
   });
 });
