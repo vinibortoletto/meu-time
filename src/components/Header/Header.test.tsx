539d98bd2463400c94204bd8c43e131fe2f3e53f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-21 19:29:04 -0300

    test: header unit test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,15 @@
-import { render, screen } from '@testing-library/react'
-import Header from './Header'
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
 
 describe('Header | component | unit test', () => {
   it('should render with success', () => {
-    render(<Header>Header</Header>)
+    render(<Header />);
+    const logo = screen.getByAltText(/bola de futebol/i);
+    const title = screen.getByRole('heading', { name: /meu time/i });
+    const button = screen.getByRole('button', { name: /sair/i });
 
-    expect(screen.getByText('Header')).toBeInTheDocument()
-  })
-})
+    expect(logo).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+});
