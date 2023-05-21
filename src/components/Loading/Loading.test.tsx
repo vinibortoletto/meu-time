4bab67a0bacce7d3c9c5fd8db81b4d291e2e07b2
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-21 20:13:20 -0300

    test: loading component

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react'
-import Loading from './Loading'
+import { render, screen } from '@testing-library/react';
+import Loading from './Loading';
 
 describe('Loading | component | unit test', () => {
   it('should render with success', () => {
-    render(<Loading>Loading</Loading>)
-
-    expect(screen.getByText('Loading')).toBeInTheDocument()
-  })
-})
+    render(<Loading />);
+    expect(
+      screen.getByAltText('desenho de um jogador fazer embaixadinha')
+    ).toBeInTheDocument();
+  });
+});
