aff31ad7c4c28cb1bc069fb4fd660fd962d7f637
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-21 00:47:22 -0300

    style: title font size

--- src/components/Title/Title.styles.ts
+++ src/components/Title/Title.styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { breakpoint } from '../../styles/breakpoint';
 import { vars } from '../../styles/variables';
 
 export const Container = styled.h2`
@@ -6,4 +7,8 @@ export const Container = styled.h2`
   font-size: 1.5rem;
   color: ${vars.color.green};
   margin-bottom: 1rem;
+
+  @media ${breakpoint('800')} {
+    font-size: 2rem;
+  }
 `;
